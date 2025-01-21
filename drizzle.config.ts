a639ab2aa0acb6839bd10096a795fa7828ae652e
samarth <samarth29jc@gmail.com>
2025-01-21 11:20:02 +0530

    before deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "drizzle-kit";
-export default defineConfig({
+// drizzle.config.ts
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import type { Config } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
+
+export default {
   schema: "./src/db/schema.ts",
+  out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
-});
+} satisfies Config;
